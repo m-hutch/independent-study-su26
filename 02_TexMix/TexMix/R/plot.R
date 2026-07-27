25785526d21bdb8c945b3d8c8f9c469e9086329d
m-hutch <maehutch@protonmail.com>
2026-07-27 10:27:03 -0500

    require sp

--- 02_TexMix/TexMix/R/plot.R
+++
@@ -1,9 +0,0 @@
-#' Plot a spatial object
-#'
-#' This is an S3 generic for spatial objects
-#'
-#' @title Plot a spatial object
-#' @param x spatial object
-#' @param ...
-#' @importFrom sp plot
-#' @export
